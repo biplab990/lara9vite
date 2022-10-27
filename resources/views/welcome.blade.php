61e61a16f3e4bb4a39e841d78df56debcaac6ee4
biplab990 <biplabuddin990@gmail.com>
2022-10-27 21:26:51 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    </head>
+    <body class="antialiased">
+        <h1 class="bg-primary">welcome laravel 9.0 vite</h1>
+
+    </body>
+</html>
